e0524a15c72bd2c7d25cc3e67bb0f2c3b6e1f98c
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2022-12-12 17:32:41 +0100

    refactored rust central

--- BleCentralInRust/README.md
+++ BleCentralInRust/README.md
@@ -1,9 +1,10 @@
 # BleCentralInRust
 Rust implementation of BLE Central. This is a work in progress.
 At the moment, it is only tested on macOS, and it presents several limitations versus its Python counterpart:
-* it does not support windows powershell scripts
 * it does not handle peripherals disconnecting
 * it does not searches continuously for peripherals
-* it does not have command line arguments to specify options.
+* it does not have command line arguments to specify options
+* scripts must be specified with their platform-specific extension (e.g. .sh for macOS, .ps1 for Windows)
+* windows powershell scripts support is untested.
 
 Nevertheless, it is a good starting point for a Rust implementation of BLE Central, and works pretty well as long as the peripheral does not disconnect.
--- BleCentralInRust/src/main.rs
+++ BleCentralInRust/src/main.rs
@@ -10,10 +10,6 @@ use std::time::Duration;
 use tokio::time;
 use uuid::Uuid;
 
-/// Whitelist of devices names.
-/// This is a list of devices that we want to connect to.
-const DEVICES_NAMES_WHITELIST: [&str; 1] = ["Pixel 3a"];
-
 /// Only devices whose services contains this UUID will be tried.
 const PERIPHERAL_SERVICE_UUID: Uuid = Uuid::from_u128(0x0000ffe0_0000_1000_8000_00805f9b34fb);
 /// UUID of the characteristic for which we should subscribe to notifications.
@@ -22,8 +18,10 @@ const SCRIPT_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x0000ffe1_0000_1000_80
 /// This is optional, and if not present, the script will be executed without any feedback.
 const STATUS_CHARACTERISTIC_UUID: Uuid = Uuid::from_u128(0x0000ffe2_0000_1000_8000_00805f9b34fb);
 
+/// UUID of a non-existent characteristic.
+/// This is used to return a characteristic when we cannot find the one we are looking for.
 const NON_EXISTENT_CHARACTERISTIC_UUID: Uuid =
-    Uuid::from_u128(0x00000000_0000_1000_8000_000000000000);
+    Uuid::from_u128(0x00000000_0000_0000_0000_000000000000);
 
 const NON_EXISTENT_CHARACTERISTIC: Characteristic = Characteristic {
     uuid: NON_EXISTENT_CHARACTERISTIC_UUID,
@@ -39,9 +37,17 @@ fn get_current_working_dir() -> Result<String, Box<dyn Error>> {
     Ok(path.to_str().unwrap().to_string())
 }
 
-fn search_characteristics_with_uuid(
+/// Filter the characteristics to only keep the ones that have the given UUIDs.
+///
+/// # Arguments
+/// * `characteristics` - The characteristics to filter.
+/// * `uuids` - The UUIDs to keep.
+///
+/// # Returns
+/// * `HashMap<Uuid, Characteristic>` - The filtered characteristics.
+fn filter_characteristics_by_uuids(
     characteristics: &BTreeSet<Characteristic>,
-    uuids: &[Uuid],
+    uuids: &Vec<Uuid>,
 ) -> HashMap<Uuid, Characteristic> {
     let mut filtered_characteristics = HashMap::new();
     for characteristic in characteristics.iter() {
@@ -52,9 +58,47 @@ fn search_characteristics_with_uuid(
     filtered_characteristics
 }
 
-/**
- * Takes the data from the notification and return the script to execute.
- */
+/// Get the script and status characteristics from the given characteristics. if they are present.
+/// If they are not present, the [NON_EXISTENT_CHARACTERISTIC] will be returned.
+///
+/// # Arguments
+/// * `characteristics` - The characteristics to filter.
+///
+/// # Returns
+/// * `(Characteristic, Characteristic)` - The script and status characteristics, respectively.
+fn get_script_and_status_characteristics_if_present(
+    characteristics: &BTreeSet<Characteristic>,
+) -> (Characteristic, Characteristic) {
+    let uuids = vec![SCRIPT_CHARACTERISTIC_UUID, STATUS_CHARACTERISTIC_UUID];
+    let filtered_characteristics = filter_characteristics_by_uuids(characteristics, &uuids);
+
+    let script_characteristic = filtered_characteristics
+        .get(&SCRIPT_CHARACTERISTIC_UUID)
+        .unwrap_or(&NON_EXISTENT_CHARACTERISTIC)
+        .clone();
+
+    let status_characteristic = filtered_characteristics
+        .get(&STATUS_CHARACTERISTIC_UUID)
+        .unwrap_or(&NON_EXISTENT_CHARACTERISTIC)
+        .clone();
+
+    (script_characteristic, status_characteristic)
+}
+
+fn script_characteristic_present_and_notifiable(script_characteristic: &Characteristic) -> bool {
+    script_characteristic.uuid != NON_EXISTENT_CHARACTERISTIC_UUID
+        && script_characteristic
+            .properties
+            .contains(CharPropFlags::NOTIFY)
+}
+
+/// Takes the data from the notification and return the script to execute.
+///
+/// # Arguments
+/// * `data` - The data received from the notification.
+///
+/// # Returns
+/// * `Result<String, Box<dyn Error>>` - The script to execute.
 fn get_script_to_execute_from_data(data: &[u8]) -> Result<String, Box<dyn Error>> {
     if data.len() == 0 {
         return Err("No data received".into());
@@ -71,11 +115,17 @@ fn get_script_to_execute_from_data(data: &[u8]) -> Result<String, Box<dyn Error>
     Ok(path.to_str().unwrap().to_string())
 }
 
-/**
- * Execute a script and return the output.
- */
+/// Execute a script and return the exit code.
+///
+/// It will also print the output of the script to the console.
+///
+/// # Arguments
+/// * `script` - The script to execute, optionally with arguments.
+///
+/// # Returns
+/// * `i32` - The exit code of the script.
 fn run_script(script: &String) -> i32 {
-    println!("AAAAARunning script {:?}", script);
+    println!("Running script {:?}", script);
 
     let parsed_script_and_args = shell_words::split(script).unwrap();
 
@@ -106,6 +156,54 @@ fn run_script(script: &String) -> i32 {
     }
 }
 
+/// Discover peripheral services and return `true` if the peripheral has the given service, `false` otherwise.
+async fn peripheral_has_service(
+    peripheral: &btleplug::platform::Peripheral,
+    service_uuid: Uuid,
+) -> bool {
+    let service_discovery_result = peripheral.discover_services().await;
+
+    if service_discovery_result.is_err() {
+        return false;
+    }
+
+    let services = peripheral.services();
+
+    for service in services {
+        if service.uuid == service_uuid {
+            return true;
+        }
+    }
+    false
+}
+
+/// Scan for peripherals using the given adapter, and return the list of peripherals or an empty vector.
+/// This function will scan for peripherals for a fixed amount of time.
+///
+/// # Arguments
+/// * `adaper` - The adapter to use for scanning.
+/// * `scan_filter` - The filter to use for scanning, or `None` to use the default filter.
+///
+/// # Returns
+/// * `Vec<btleplug::platform::Peripheral>` - The list of peripherals found.
+async fn scan(
+    adaper: &btleplug::platform::Adapter,
+    scan_filter: Option<&ScanFilter>,
+) -> Vec<btleplug::platform::Peripheral> {
+    let default_filter = &ScanFilter::default();
+    let scan_filter = scan_filter.unwrap_or(default_filter);
+
+    adaper
+        .start_scan(scan_filter.to_owned())
+        .await
+        .expect("Can't scan BLE adapter for connected devices...");
+
+    time::sleep(SCAN_FOR_PERIPHERALS_TIMEOUT).await;
+
+    // unwrap peripherals or empty vector
+    adaper.peripherals().await.unwrap_or(vec![])
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
@@ -116,99 +214,66 @@ async fn main() -> Result<(), Box<dyn Error>> {
         eprintln!("No Bluetooth adapters found");
     }
 
+    // create a scan filter to only scan for devices with the given service UUID
+    let mut scan_filter = ScanFilter::default();
+    scan_filter.services = vec![PERIPHERAL_SERVICE_UUID];
+
     for adapter in adapter_list.iter() {
         println!("Starting scan...");
-        adapter
-            .start_scan(ScanFilter::default())
-            .await
-            .expect("Can't scan BLE adapter for connected devices...");
-        time::sleep(SCAN_FOR_PERIPHERALS_TIMEOUT).await;
-        let peripherals = adapter.peripherals().await?;
+        let peripherals = scan(adapter, Some(&scan_filter)).await;
 
         if peripherals.is_empty() {
             eprintln!("->>> BLE peripheral devices were not found, sorry. Exiting...");
-        } else {
-            // All peripheral devices in range.
-            for peripheral in peripherals.iter() {
-                let properties = peripheral.properties().await?;
-                let is_connected = peripheral.is_connected().await?;
-                let local_name = properties
-                    .unwrap()
-                    .local_name
-                    .unwrap_or(String::from("(peripheral name unknown)"));
-                println!(
-                    "Peripheral {:?} is connected: {:?}",
-                    &local_name, is_connected
-                );
+            continue;
+        }
 
-                if !DEVICES_NAMES_WHITELIST.iter().any(|&x| x == local_name) {
-                    println!(
-                        "Skipping device {:?} because it's not in the whitelist",
-                        local_name
-                    );
+        // All peripheral devices in range.
+        for peripheral in peripherals.iter() {
+            let properties = peripheral.properties().await?;
+            let is_connected = peripheral.is_connected().await?;
+
+            let local_name = properties
+                .unwrap()
+                .local_name
+                .unwrap_or(String::from("(peripheral name unknown)"));
+            println!(
+                "Peripheral {:?} is connected: {:?}",
+                &local_name, is_connected
+            );
+
+            // Connect if we aren't already connected.
+            if !is_connected {
+                if let Err(err) = peripheral.connect().await {
+                    eprintln!("Error connecting to peripheral, skipping: {}", err);
                     continue;
                 }
+            }
+            let is_connected = peripheral.is_connected().await?;
+            println!(
+                "Now connected ({:?}) to peripheral {:?}...",
+                is_connected, &local_name
+            );
 
-                if !is_connected {
-                    // Connect if we aren't already connected.
-                    if let Err(err) = peripheral.connect().await {
-                        eprintln!("Error connecting to peripheral, skipping: {}", err);
-                        continue;
-                    }
-                }
-                let is_connected = peripheral.is_connected().await?;
-                println!(
-                    "Now connected ({:?}) to peripheral {:?}...",
-                    is_connected, &local_name
-                );
-                peripheral.discover_services().await?;
-                let services = peripheral.services();
-                let mut found_service = false;
-                if services.is_empty() {
-                    eprintln!(
-                        "->>> No services found for peripheral device: {:?}",
-                        peripheral.address()
-                    );
-                } else {
-                    // All services for the current peripheral device.
-                    for service in services.iter() {
-                        if service.uuid == PERIPHERAL_SERVICE_UUID {
-                            println!("->>> Found service: {:?}", service);
-                            found_service = true;
-                            break;
-                        }
-                    }
-                }
-                if found_service {
-                    let characteristics = peripheral.characteristics();
+            let found_service = peripheral_has_service(peripheral, PERIPHERAL_SERVICE_UUID).await;
 
-                    let searched_characteristics = search_characteristics_with_uuid(
-                        &characteristics,
-                        &vec![STATUS_CHARACTERISTIC_UUID, SCRIPT_CHARACTERISTIC_UUID],
-                    );
+            if found_service {
+                // get script and status characteristics, using get_script_and_status_characteristics_if_present
+                let (script_characteristic, status_characteristic) =
+                    get_script_and_status_characteristics_if_present(&peripheral.characteristics());
 
-                    let script_characteristic = searched_characteristics
-                        .get(&SCRIPT_CHARACTERISTIC_UUID)
-                        .unwrap_or(&NON_EXISTENT_CHARACTERISTIC);
-                    let status_characteristic = searched_characteristics
-                        .get(&STATUS_CHARACTERISTIC_UUID)
-                        .unwrap_or(&NON_EXISTENT_CHARACTERISTIC);
-
-                    if script_characteristic == &NON_EXISTENT_CHARACTERISTIC
-                        || !script_characteristic
-                            .properties
-                            .contains(CharPropFlags::NOTIFY)
-                    {
+                match script_characteristic_present_and_notifiable(&script_characteristic) {
+                    false => {
                         println!(
                             "->>> Script characteristic not found or not notifyable, skipping..."
                         );
-                    } else {
+                    }
+                    true => {
                         println!(
                             "Subscribing to characteristic {:?}",
                             script_characteristic.uuid
                         );
                         peripheral.subscribe(&script_characteristic).await?;
-                        // Print the first 4 notifications received.
+
                         let mut notification_stream = peripheral.notifications().await?;
                         // Process while the BLE connection is not broken or stopped.
                         while let Some(data) = notification_stream.next().await {
@@ -220,7 +285,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             let cmd_string = get_script_to_execute_from_data(&data.value)?;
 
                             // Inform the peripheral that we are about to execute the script.
-                            if *status_characteristic != NON_EXISTENT_CHARACTERISTIC {
+                            if status_characteristic != NON_EXISTENT_CHARACTERISTIC {
                                 peripheral.read(&status_characteristic).await?;
                             }
 
@@ -228,13 +293,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             let exit_code = run_script(&cmd_string);
 
                             // Inform the peripheral that we have finished executing the script.
-                            if *status_characteristic != NON_EXISTENT_CHARACTERISTIC {
+                            if status_characteristic != NON_EXISTENT_CHARACTERISTIC {
                                 let data = exit_code.to_string().as_bytes().to_vec();
-
-                                println!(
-                                    "Writing to characteristic {:?} value {:?}",
-                                    status_characteristic.uuid, data
-                                );
 
                                 peripheral
                                     .write(
@@ -246,10 +306,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
                             }
                         }
                     }
-                }
-                println!("Disconnecting from peripheral {:?}...", local_name);
-                peripheral.disconnect().await?;
+                };
             }
+            println!("Disconnecting from peripheral {:?}...", local_name);
+            peripheral.disconnect().await?;
         }
     }
     Ok(())
